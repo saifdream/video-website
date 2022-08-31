3b0c7b6b4d41c4ca618fbbb0398ef2845c280463
Md Saiful Islam <saif89.2012@gmail.com>
2022-09-01 00:14:30 +0600

    init

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import filterReducer from "../features/filter/filterSlice";
+import relatedVideosReducer from "../features/relatedVideos/relatedVideosSlice";
+import tagsReducer from "../features/tags/tagsSlice";
+import videoReducer from "../features/video/videoSlice";
+import videosReducer from "../features/videos/videosSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: {
+        videos: videosReducer,
+        tags: tagsReducer,
+        video: videoReducer,
+        relatedVideos: relatedVideosReducer,
+        filter: filterReducer,
+    },
 });
