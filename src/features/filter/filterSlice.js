b04b7eb4a13bea1f0f9b8bc04838603de9a35e74
Md Saiful Islam <saif89.2012@gmail.com>
2022-09-01 23:13:38 +0600

    current page bug fix while filter & search

--- src/features/filter/filterSlice.js
+++ src/features/filter/filterSlice.js
@@ -14,19 +14,23 @@ const filterSlice = createSlice({
     reducers: {
         tagSelected: (state, action) => {
             state.tags.push(action.payload);
+            state.currentPage = 1;
         },
         tagRemoved: (state, action) => {
             const indexToRemove = state.tags.indexOf(action.payload);
 
             if (indexToRemove !== -1) {
                 state.tags.splice(indexToRemove, 1);
+                state.currentPage = 1;
             }
         },
         searched: (state, action) => {
             state.search = action.payload;
+            state.currentPage = 1;
         },
         authorSelected: (state, action) => {
             state.author = action.payload;
+            state.currentPage = 1;
         },
         setCurrentPage: (state, action) => {
             state.currentPage = action.payload;
